0710c59938b320c8773b836006878d82faef4100
Daniel Jones <daniel.jones@engineerbetter.com>
2016-08-08 13:45:35 +0100

    add script to demonstrate things not working

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # hazelcast-node-test
+
+Quick script to demonstrate that a Hazelcast node running as a CF app cannot be communicated to, without TCP routing or container networking. Neither of these features are enabled in mainstream PCF at present.
+
+## To run
+
+The script assumes that you're logged into Cloud Foundry, and are targetting a space where you can push apps.
+
+`CF_HOST` should be the app domain of your Cloud Foundry.
+
+`NO_ROUTE_ADDR` is an address to try testing the Hazelcast node when it's given no HTTP route; perhaps an IP address of the Diego cell you think it's running on?
+
+```
+CF_HOST=apps.your-cf.com \
+NO_ROUTE_ADDR=123.123.123.123 \
+./test.sh
+```
+
+The script will output a summary at the end, and takes a few minutes to run.
+
+It is expected that all tests will fail, as containers in Cloud Foundry cannot communicate with each other directly. TCP communications to apps in Cloud Foundry are only possible if TCP routing is enabled (not the case for most instances), and a TCP route created.
---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x
+
+: ${CF_HOST:?"CF_HOST is required, which should be the app domain that apps will run under"}
+: ${NO_ROUTE_ADDR:?"NO_ROUTE_ADDR is required, which should be the host or IP that you want to try hitting a routeless node on"}
+
+pushd node-no-route
+  mvn clean package
+  cf push
+  HAZELCAST_ADDR=$NO_ROUTE_ADDR mvn verify
+  no_route_exit=$?
+  cf delete node -f
+popd
+
+pushd node-http-route
+  mvn clean package
+  cf push
+  HAZELCAST_ADDR=node.$CF_HOST mvn verify
+  http_route_exit=$?
+popd
+
+pushd hazelcast-example-app
+  mvn clean package
+  cf push --no-start
+  cf set-env hazelcast-example-app ADDRESSES node.$CF_HOST
+  cf start hazelcast-example-app -t 30
+  example_start=$?
+popd
+
+cf delete hazelcast-example-app -f
+cf delete node -f
+
+if [ $no_route_exit -ne 0 ]; then
+  echo "Test against node running with no route did not pass"
+fi
+
+if [ $http_route_exit -ne 0 ]; then
+  echo "Test against node running with HTTP route did not pass"
+fi
+
+if [ $example_start -ne 0 ]; then
+    echo "Example app failed to start, probably because it could not talk to the Hazelcast node"
+fi
